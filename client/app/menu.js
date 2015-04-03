0f16188b7ce0c6d98a100181797ee3855f6bed1b
Charlie Depman <cdepman@gmail.com>
2015-04-03 13:03:23 +0300

    adds modals

--- client/app/menu.js
+++ client/app/menu.js
@@ -2,14 +2,16 @@ var width = window.innerWidth,
   height = window.innerHeight,
   padding = 3, // separation between nodes
   menuColor = "#cc6666", // fill color
-  menuOffset = 110, // offset from middle
+  menuOffset = height < 750 ? 50 : 110, // offset from middle
+  radiusOffset = height < 750 ? .7 : 1,
+  textSize = height < 750 ? '1em' : '1.3em',
   textOffset = 6, // offset for text inside circle elements
-  maxRadius = 50, // radii
-  myWork =   { radius: 50, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "My Work", class: "menu my-work" },
-  connect = { radius: 49, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "Connect", class: "menu connect" },
-  aboutMe =   { radius: 37, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "About", class: "menu about-me" },
-  blog =   { radius: 32, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "Blog", class: "menu blog" },
-  cv =   { radius: 24, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "CV", class: "menu cv" }
+  maxRadius = 50 * radiusOffset, // radii
+  myWork =   { radius: 50 * radiusOffset, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "My Work", class: "modal-trigger menu my-work", textSize: textSize },
+  connect = { radius: 49 * radiusOffset, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "Connect", class: "modal-trigger menu connect", textSize: textSize },
+  aboutMe =   { radius: 37 * radiusOffset, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "About", class: "modal-trigger menu about-me", textSize: textSize },
+  blog =   { radius: 32 * radiusOffset, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "Blog", class: "modal-trigger menu blog", textSize: textSize },
+  cv =   { radius: 24 * radiusOffset, color: menuColor, cx: width/2, cy: height/2 + menuOffset, name: "CV", class: "modal-trigger menu cv", textSize: textSize }
 
 var n = 5, // total number of nodes
   m = 1; // number of distinct clusters
@@ -33,6 +35,7 @@ var svg = d3.select("body").append("svg")
 var circle = svg.selectAll("circle")
   .data(nodes)
   .enter().append("circle")
+  .attr("href", "#modal1")
   .attr("class", function(d) {return d.class})
   .attr("r", function(d) { return d.radius; })
   .style("fill", function(d) { return d.color; })
@@ -55,7 +58,7 @@ var textLabels = text
   .attr("text-anchor", "middle")
   .attr("font-family", "'Raleway', sans-serif")
   .attr("font-weight", "400")
-  .attr("font-size", "1.3em")
+  .attr("font-size", function(d){return d.textSize})
   .attr("fill", "black");
 
 function tick(e) {
@@ -114,20 +117,26 @@ function resize(e){
   var height = window.innerHeight;
   svg.attr('width', width);
   svg.attr('height', height);
+
+  menuOffset = height > 750 ? 110 : 50;
+  radiusOffset = height > 750 ? 1 : .7;
+
   nodes.forEach(function(node){
     node.cx = width/2;
     node.cy = height/2 + menuOffset;
+    node.radius = node.radius * radiusOffset;
   })
+
   force.size([width, height]).resume();
 }
 
 
 // enter connect icons set up listener
 var connectorIcons = [
-  '<i id="email" class="connect-icon fa fa-envelope-square fa-5x"></i>',
-  '<i id="linked-in" class="connect-icon fa fa-linkedin-square fa-5x"></i>',
-  '<i id="facebook" class="connect-icon fa fa-facebook-official fa-5x"></i>',
-  '<i id="github" class="connect-icon fa fa-github-square fa-5x"></i>'
+  '<i id="email" class="hvr-grow connect-icon fa fa-envelope-square fa-5x"></i>',
+  '<i id="linked-in" class="hvr-grow connect-icon fa fa-linkedin-square fa-5x"></i>',
+  '<i id="facebook" class="hvr-grow connect-icon fa fa-facebook-official fa-5x"></i>',
+  '<i id="github" class="hvr-grow connect-icon fa fa-github-square fa-5x"></i>'
 ];
 
 connectorIcons.forEach(function(icon){
@@ -136,7 +145,7 @@ connectorIcons.forEach(function(icon){
 $('.connect-icon').toggle();
 
 var connect = function(){
-  $('.connect-icon').toggle("slow");
+  $('.connect-icon').fadeToggle("slow");
 };
 
 $('.connect').on('click', function(){
@@ -145,10 +154,24 @@ $('.connect').on('click', function(){
 
 // enter about description and set up listener
 var about = "<p id='about'>My passion for using tech for good stems from my experiences living in 15 of the world’s busiest cities. My background includes environmental advocacy, fundraising, and teaching. I also enjoy creative writing (mostly sci-fi) and once considered it as a vocation. But I had a revelation: why not learn how to turn all those exciting ideas into reality instead of just writing about them?<br><br>I talked to friends and mentors and rediscovered coding.<br><br>...I’ve been in love ever since.</p>"
-
-$('body').append(about);
-$('#about').toggle();
-
 $('.about-me').on('click', function(){
-  $('#about').toggle("slow");  
+  $('#lean_overlay').fadeIn();
+  $('#modal1').fadeIn();
+})
+$('body').prepend("<div id='lean_overlay'></div>");
+$('#lean_overlay').on('click',function(){
+  $('#lean_overlay').fadeOut();
+  $('#modal1').fadeOut();
+})
+
+$('.modal-close').on('click',function(){
+  $('#lean_overlay').fadeOut();
+  $('#modal1').fadeOut();
+})
+
+var cvElement = "<object id='cv' data='./assets/CharlieDepmanResumeV2.pdf' type='application/pdf' width='100%' height='100%'> <p>It appears you don't have a PDF plugin for this browser. No biggie... you can <a href='myfile.pdf'>click here to download the PDF file.</a></p></object>"
+
+$('.cv').on('click', function(){
+  $('#lean_overlay').fadeIn();
+  $('#modal2').fadeIn();
 });
